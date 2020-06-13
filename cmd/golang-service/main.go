a451071628458d5952a1d578e5112553e7f29899
anggriawan <anggriawanrilda88@gmail.com>
2020-06-13 11:54:51 +0000

    first commit

---
+++ cmd/golang-service/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "log"
+
+func main() {
+	log.Println("HELLO this is golang docker template")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module gitlab.com/anggri-microservice/golang-service
+
+go 1.13
